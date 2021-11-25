4966de68a4bde09de272ae3edc01ba44eda8daaf
ubg11111 <ubg11111@naver.com>
2021-11-25 19:22:50 +0900

    Controls work

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,10 +4,19 @@ const muteBtn = document.getElementById("mute");
 const volumeRange = document.getElementById("volume");
 const currenTime = document.getElementById("currenTime");
 const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreen");
+const videoContainer = document.getElementById("videoContainer");
+const videoControls = document.getElementById("videoControls");
 
 
+let controlsTimeout = null;
+let constrolsMovementTimeout = null;
 let volumeValue = 0.5;
 video.volume = volumeValue;
+
+// 포맷타임변수에 세컨드 어규먼트를 적용 new Date 이벤트를 각초에 * 1000ms 적용하여 초를 문자식으로 변환 substr을 통해 노출할 필요 문자열을 잘라 가져옴
+const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substr(14, 5)
 
 
 // handlePlayClick 함수생성 조건문 paused(멈춤) 플레이 조정
@@ -43,13 +52,49 @@ const handleVolumeChange = (event) => {
 }
 
 const handleLoadedMetadata = () => {
-  totalTime.innerText = Math.floor(video.duration);
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+  timeline.max = Math.floor(video.duration);
 };
 
 const handleTimeUpdate = () => {
-  currenTime.innerText = Math.floor(video.currentTime);
-  console.log(video.currentTime);
+  currenTime.innerText = formatTime(Math.floor(video.currentTime));
+  timeline.value = Math.floor(video.currentTime);
 }
+
+const handleTimelineChange = (event) => {
+  const { target: { value }, } = event;
+  video.currentTime = value;
+}
+
+const handleFullScreen = () => {
+  const fullscreen = document.fullscreenElement;
+  if (fullscreen) {
+    document.exitFullscreen();
+    fullScreenBtn.innerText = "Enter Full Screen";
+  } else {
+    videoContainer.requestFullscreen();
+    fullScreenBtn.innerText = "Exit Full Screen";
+  }
+}
+
+const hideControls = () => videoControls.classList.remove("showing");
+
+const handleMouseMove = () => {
+  if (controlsTimeout) {
+    clearTimeout(controlsTimeout);
+    controlsTimeout = null;
+  }
+  if (constrolsMovementTimeout) {
+    clearTimeout(constrolsMovementTimeout);
+    constrolsMovementTimeout = null;
+  }
+  videoControls.classList.add("showing");
+  constrolsMovementTimeout = setTimeout(hideControls, 3000)
+}
+
+const handleMouseLeave = () => {
+  controlsTimeout = setTimeout(hideControls, 3000);
+};
 
 // 비디오재생 클릭 이벤트
 playBtn.addEventListener("click", handlePlayClick);
@@ -57,3 +102,7 @@ muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimelineChange);
+fullScreenBtn.addEventListener("click", handleFullScreen);
+video.addEventListener("mousemove", handleMouseMove);
+video.addEventListener("mouseleave", handleMouseLeave);
