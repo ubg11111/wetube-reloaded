b4a5eda4f9937614cf1942bcdb9f8fea64f948f3
ubg11111 <ubg11111@naver.com>
2021-12-16 19:20:38 +0900

    commit

--- src/server.js
+++ src/server.js
@@ -31,11 +31,6 @@ app.use(flash());
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"), express.static("node_modules/@ffmpeg/core/dist"));
-app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
-  res.header("Cross-Origin-Opener-Policy", "same-origin");
-  next();
-});
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
